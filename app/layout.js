494e54d30243842b7c48776192874df00629542c
vonvernice <frandalozzo@gmail.com>
2024-04-15 20:49:17 -0300

    feat:adicionando modal

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import { Providers } from "./providers";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,8 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='dark'>
+
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
